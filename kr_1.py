e44be13dd2d6f34631226e4558a60c17fb507e3e
24dima1 <53940999+24dima1@users.noreply.github.com>
2025-01-14 10:36:05 +0300

    Update kr_1_1.py

--- kr_1.py
+++ kr_1.py
@@ -1,87 +1,124 @@
 #task 1
-a = 1
-b = 2
-for i in range(a, b+1):
-    print(i)
+
+def number_cruncing(a, b):
+    for i in range(a, b+1):
+        print(i)
+    return i
+number_cruncing(1, 4)
 
 #task 2
-a = 2
-b = 1
-if (a<b):
-    for i in range(a, b + 1):
-        print(i)
-else:
-    for i in range(a,  b-1, -1):
-        print(i)
+
+def fun_sort(a, b):
+    if (a<b):
+        for i in range(a, b + 1):
+            print(i)
+    else:
+        for i in range(a,  b-1, -1):
+            print(i)
+    return i 
+fun_sort(2, 1)
+
 #task 3
-a = 6
-b = 1
-for i in range(a, b - 1, -1):
-    if i % 2 != 0:
-        print(i)
+
+def fun_sort_odd(a, b):
+    for i in range(a, b - 1, -1):
+        if i % 2 != 0:
+            print(i)
+    return i
+fun_sort_odd(6, 1)
+
 #task 4
-N = int(input(" Введите количество чисел: "))
-sum = 0
-for i in range (0, N):
-    sum += int(input(" Введите число: "))
-print(sum)
+
+def fun_sum():
+    N = int(input(" Введите количество чисел: "))
+    sum = 0
+    for i in range(0, N):
+        sum += int(input(" Введите число: "))
+    print(sum)
+    return sum
+fun_sum()
 
 #task 5
 
-N = int(input(" Введите число n: "))
-sum = 0
-for i in range (1, N + 1):
-    sum += i ** 3
-print(f"Сумма чисел равна: {sum}")
+def fun_sum_num():
+
+    N = int(input(" Введите число n: "))
+    sum = 0
+    for i in range (1, N + 1):
+        sum += i ** 3
+    print(f"Сумма чисел равна: {sum}")
+
+fun_sum_num()
+
 
 #task 6
-n = int(input(" Введите n факториала: "))
-fact = 1
-for i in range (1, n + 1):
-    fact *= i
-print(f"Сумма чисел равна: {fact} ")
+
+def fun_fact():
+
+    n = int(input(" Введите n факториала: "))
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    print(f"Сумма чисел равна: {fact} ")
+
+fun_fact()
 
 #task 7
-n = int(input("Введите n ряда:"))
-sum = 0
-fact = 1
-for i in range (1, n + 1):
-    fact *= i
-    sum += fact
-print(f"Сумма чисел равна: {sum}" )
+
+def fun_fact_sum():
+    n = int(input("Введите n ряда:"))
+    sum = 0
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+        sum += fact
+    print(f"Сумма чисел равна: {sum}")
+    return sum
+fun_fact_sum()
 
 #task 8
-n = int(input(" Введите n ступенек: "))
-for i in range(1, n + 1):
+
+def fun_step():
+    n = int(input(" Введите n ступенек: "))
+    for i in range(1, n + 1):
         step = ""
         for j in range(1, i + 1):
             step += str(j)
         print(step)
+    return step
+fun_step()
 
-#task 9
+#task 9.1
 
-n = int(input(" Введите количество чисел из ряда Фибоначчи : "))
-fib1, fib2 = 0, 1
-total_sum = 0
-for i in range(n):
-    total_sum += fib1
-    fib1, fib2 = fib2, fib1 + fib2
 
-print(f"Сумма чисел равна: {total_sum}")
+def fun_sum_fib():
+    n = int(input(" Введите количество чисел из ряда Фибоначчи : "))
+    fib1, fib2 = 0, 1
+    total_sum = 0
+    for i in range(n):
+        total_sum += fib1
+        fib1, fib2 = fib2, fib1 + fib2
 
-#task 10
+    print(f"Сумма чисел равна: {total_sum}")
+    return total_sum
+fun_sum_fib()
 
-n = int(input(" Введите количество количество чисел из ряда Фибоначчи: "))
-k = int(input(" Введите -порядковый номер в ряду: "))
+#task 10.1
 
-fib1, fib2 = 0, 1
-sum = 0
-cur = 1
+def fun_sum_fib_num():
+    n = int(input(" Введите количество количество чисел из ряда Фибоначчи: "))
+    k = int(input(" Введите -порядковый номер в ряду: "))
 
-for i in range(k + n - 1):
-    if cur >= k:
-        sum += fib1
-    fib1, fib2 = fib2, fib1 + fib2
-    cur += 1
+    fib1, fib2 = 0, 1
+    sum = 0
+    cur = 1
 
-print(f"Сумма чисел равна: {sum}")
+    for i in range(k + n - 1):
+        if cur >= k:
+            sum += fib1
+        fib1, fib2 = fib2, fib1 + fib2
+        cur += 1
+
+    print(f"Сумма чисел равна: {sum}")
+    return sum
+fun_sum_fib_num()
